728a2c68795ec1b16366a7bb02c3ccefe414e59c
Fidel Caicedo <869@holbertonschool.com>
2019-09-10 20:22:50 +0000

    adding sixth point

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for first in range(0, 10):
+    for second in range(0, 10):
+        if first < second:
+            if first != 8 and second != 9:
+                print("{}{}".format(first, second), end=", ")
+            if first == 8 and second == 9:
+                print("{}{}".format(first, second))
