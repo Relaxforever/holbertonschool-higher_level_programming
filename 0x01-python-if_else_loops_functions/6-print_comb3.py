7a00e726b673fafe593e95a1b55fa4525b7ca4a9
Fidel Caicedo <869@holbertonschool.com>
2019-09-10 20:29:11 +0000

    correcting sixth point

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for first in range(0, 10):
     for second in range(0, 10):
         if first < second:
+            if first != 8:
                 print("{}{}".format(first, second), end=", ")
-        if first == 8 and second == 9:
+            if first == 8 and second == 9:
                 print("{}{}".format(first, second))
