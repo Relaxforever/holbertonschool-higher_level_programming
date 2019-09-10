05ed8f92068f61d0f5775a994e63fe6851dc71f6
root <869@holbertonschool.com>
2019-09-10 02:05:48 +0000

    Uploading tenth point

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+*check_cycle - a program to check if a linked list has a cycle in it or not
+*@list: the linked list We will be given
+*Return: 0 if there is no loop, 1 if there is a loop.
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *first;
+	listint_t *third;
+	int cont;
+
+	first = list;
+	third = first->list;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	for (cont = 0; first && third; cont++)
+	{
+		first = first->next;
+		third = third->next;
+		if (first == third)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
