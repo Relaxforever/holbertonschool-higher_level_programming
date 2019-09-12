92d5f2617b85120d0b99c916fca066e29c240da5
Fidel Caicedo <869@holbertonschool.com>
2019-09-12 19:43:51 +0000

    changing from python2 to python3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 if __name__ == "__main__":
-    cont = 0
     from sys import argv
+    cont = 0
     for i in range(len(argv)):
         if argv[i] != argv[0]:
             cont += int(argv[i])
