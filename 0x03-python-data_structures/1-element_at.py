cedca00c2266bdfabe40fe1c3792673d897a47c5
Fidel Caicedo <869@holbertonschool.com>
2019-09-17 17:12:09 +0000

    changing if logic

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     len_of_list = len(my_list)
-    if idx > len_of_list or idx < 0:
+    if idx >= len_of_list or idx < 0:
         return None
     return my_list[idx]
