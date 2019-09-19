3ce1170ea473933891a80f48b0c70cf029b20d4c
root <869@holbertonschool.com>
2019-09-19 22:28:12 +0000

    Roma Invicta!

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or if type(roman_string) is str:
+    if not roman_string or not type(roman_string) is str:
         return 0
     value = 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
