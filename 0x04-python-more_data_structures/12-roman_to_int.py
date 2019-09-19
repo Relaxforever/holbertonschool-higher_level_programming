e81090fc5daa677f381f3878528351289aed6289
root <869@holbertonschool.com>
2019-09-19 22:24:29 +0000

    Roma Invicta!

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    value = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    revert = roman_string[::-1]
+    for invict in range(len(revert)):
+        if invict != 0 and roman[revert[invict - 1]] > roman[revert[invict]]:
+            value = value - roman[revert[invict]]
+        else:
+            value = value + roman[revert[invict]]
+    return value
