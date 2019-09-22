a802d14c25300233675557726da4706411aea82a
Fidel Caicedo <869@holbertonschool.com>
2019-09-22 16:03:03 -0500

    uploading 102 complex point

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if not value:
+        return None
+    for x in list(a_dictionary):
+        if x == value:
+            a_dictionary.pop(value)
+            break
+    return a_dictionary
