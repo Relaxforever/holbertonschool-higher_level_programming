d83ffba9a945326d231ebe42a42e0bd2969d8ddd
Fidel Caicedo <869@holbertonschool.com>
2019-09-30 18:04:37 +0000

    adding repository and directory

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    y = 0
+    while (y < x):
+        try:
+            print("{}".format(my_list[y]), end="")
+            y = y + 1
+        except IndexError:
+            break
+        else:
+            break
+    print()
+    return y
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+Hit me harder daddy
