dc92be4a8907f418fcab977fa82be1a4a2ed2958
root <869@holbertonschool.com>
2019-10-01 02:25:24 +0000

    adding the return True again with the esle

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,10 +5,11 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value), end='\n')
+    except ValueError as Error:
+        print("Exception: {}".format(Error), file=sys.stderr)
+        return False
+    except TypeError as Error:
+        print("Exception: {}".format(Error), file=sys.stderr)
+        return False
+    else:
         return True
-    except ValueError:
-        print("Exception: {}".format(Error), file=sys.stderr)
-        return False
-    except TypeError:
-        print("Exception: {}".format(Error), file=sys.stderr)
-        return False
