92195352cd48dc8035e6caba32dd61fe7d6c7da3
root <869@holbertonschool.com>
2019-10-01 02:33:28 +0000

    uploading 101 point

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as Error:
+        print("Exception: {}".format(Error), file=sys.sdterr)
+        return None
