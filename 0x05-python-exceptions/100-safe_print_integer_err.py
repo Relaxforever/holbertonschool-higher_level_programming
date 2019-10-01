c0c557206e0c0d2ed31453b2b66baa4cfb4acd8a
root <869@holbertonschool.com>
2019-10-01 02:20:31 +0000

    adding 100 safe print integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        print("Exception: {}".format(Error), file=sys.stderr)
+        return False
+    except TypeError:
+        print("Exception: {}".format(Error), file=sys.stderr)
+        return False
+    else:
+        return True
