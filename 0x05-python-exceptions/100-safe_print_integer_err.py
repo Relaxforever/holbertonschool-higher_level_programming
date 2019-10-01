d14d74fb5cfa69b60b464c4a43bd960275dcf0ef
root <869@holbertonschool.com>
2019-10-01 02:23:27 +0000

    adding the return True without Else statement

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,11 +5,10 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value), end='\n')
+        return True
     except ValueError:
         print("Exception: {}".format(Error), file=sys.stderr)
         return False
     except TypeError:
         print("Exception: {}".format(Error), file=sys.stderr)
         return False
-    else:
-        return True
