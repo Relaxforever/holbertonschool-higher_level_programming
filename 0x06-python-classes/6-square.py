8a9c7f3bce9249f60fb84040e7e1ca1002093332
Fidel Caicedo <869@holbertonschool.com>
2019-10-01 22:30:34 -0500

    uploading sixth point updated without asking innecesary error handling in the init, didn't know that's check in the setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,12 +5,6 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.position = position
-        if position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
     def area(self):
         return self.__size * self.__size
@@ -34,11 +28,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0 or not isinstance(value, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
+        if len(value) != 2 or (value[0] < 0 or value[1]) < 0 or not isinstance(value, tuple) or \
+        not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
