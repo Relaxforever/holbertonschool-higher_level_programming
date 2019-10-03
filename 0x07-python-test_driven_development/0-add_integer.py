8791d094c70f69c8cacbdf1e79e26a08aa0e5276
root <869@holbertonschool.com>
2019-10-03 22:37:40 +0000

    adding everything till because my pc is going to die

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if not isinstance(a, int):
+        if isinstance(a, float):
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        if isinstance(b, float):
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return a + b
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    for row in matrix:
+        print("hola soy la row {} mi contenido es {} y mi len es ", row)
