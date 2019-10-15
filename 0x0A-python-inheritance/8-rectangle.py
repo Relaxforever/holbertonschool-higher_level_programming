0266d6e48c4650a6106e71872374fc6cfc81724f
Fidel Caicedo <869@holbertonschool.com>
2019-10-15 20:44:53 +0000

    adding eight again with super

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,17 +10,16 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """ validates the value variable """
-        if not type(value) ==  int:
+        if not type(value) == int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
 
-
 class Rectangle(BaseGeometry):
     """ a class that inherits from base"""
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
