fae482b26122cbe83ca0a6564d047833369d1d68
Fidel Caicedo <869@holbertonschool.com>
2019-10-16 22:21:58 +0000

    adding more points

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module has a student Class """
+
+
+class Student:
+    """ A student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves the dict """
+        if attrs is None:
+            return self.__dict__
+        else:
+            dict_2 = dict()
+            for key in attrs:
+                if key in self.__dict__.keys():
+                    dict_2[key] = self.__dict__[key]
+            return dict_2
