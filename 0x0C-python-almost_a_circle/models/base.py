359bbbf63a663e0e480535cafc575a4263f2328a
Fidel Caicedo <869@holbertonschool.com>
2019-10-22 14:56:01 -0500

    uploading base update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ change from dict to json """
         if len(list_dictionaries) == 0 or list_dictionaries is None:
             return dumps([])
         else:
