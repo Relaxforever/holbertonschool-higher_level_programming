cf7630c2824b1325b15f5f1f52985bb0fcae3d88
Fidel Caicedo <869@holbertonschool.com>
2019-11-26 18:55:05 +0000

    uploading fixed fifth point correct name of table

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- creates a table in a database
 -- if it doesn't exist create it
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT '1' UNIQUE, name VARCHAR(256) NOT
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT '1' UNIQUE, name VARCHAR(256) NOT
 NULL);
