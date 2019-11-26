9e977bbb01b031650f5578bedc676073710d0fd0
Fidel Caicedo <869@holbertonschool.com>
2019-11-26 23:11:40 +0000

    uploading tenth point

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- creates a table in a database
+-- if it doesn't exist create it
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
+INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.genre_id ORDER BY
+tv_shows.title, tv_show_genres.genre_id DESC;
