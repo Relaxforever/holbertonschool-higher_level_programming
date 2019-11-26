be27ac2a4c963593bb90f9bb8b9790b2c68d7fa5
Fidel Caicedo <869@holbertonschool.com>
2019-11-26 23:14:56 +0000

    uploading tenth point

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 -- if it doesn't exist create it
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
 INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.genre_id ORDER BY
-tv_shows.title, tv_show_genres.genre_id DESC;
+tv_shows.title, tv_show_genres.genre_id;
