f4a936bdf93ce7266904b2a942348e8065aea427
Fidel Caicedo <869@holbertonschool.com>
2019-11-26 17:25:49 +0000

    uploading first point

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates MySQL server user
+-- if it already exist, the script should not fail
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
