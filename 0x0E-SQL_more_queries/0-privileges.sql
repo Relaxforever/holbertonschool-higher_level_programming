c872ab0e3c722089cce03fca2ffa7cc5bf2a788b
Fidel Caicedo <869@holbertonschool.com>
2019-11-26 17:16:25 +0000

    uploading the fixed correct 0 point with localhost

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- List the privileges of the users
 -- user_0d1 and user_0d2
-SHOW GRANTS FOR user_0d_1; SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
