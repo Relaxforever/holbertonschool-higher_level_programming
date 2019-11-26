02e9db1d77ba649b95ce42794ea8ef6b732cd7fa
Fidel Caicedo <869@holbertonschool.com>
2019-11-26 17:38:29 +0000

    specifying data base

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 -- if if already exist then don't do it
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2. * TO 'user_0d_2'@'localhost';
