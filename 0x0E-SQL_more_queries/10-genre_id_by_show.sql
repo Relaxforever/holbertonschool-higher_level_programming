bbaadc41a0830545449c1d145b363bfe1a136c8e
Fidel Caicedo <869@holbertonschool.com>
2019-11-26 23:22:36 +0000

    uploading fixed tenth point correct looking taf

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- creates a table in a database
 -- if it doesn't exist create it
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
-INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.genre_id ORDER BY
-tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
