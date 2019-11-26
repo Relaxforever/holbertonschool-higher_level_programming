13a6ae73822b9cfae785aed662d126a6ba3623fc
Fidel Caicedo <869@holbertonschool.com>
2019-11-26 18:34:15 +0000

    updating hte when not null

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,4 @@
 -- creates a table in a database
 -- if it doesn't exist create it
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT
+NULL);
