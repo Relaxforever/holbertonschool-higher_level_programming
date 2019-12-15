24ea4cbb66312f4272e629e3990704d254d0c504
Fidel Caicedo <869@holbertonschool.com>
2019-12-15 11:39:09 -0500

    uploading secure third point

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module gets a database and prints it in ascending order """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(host="localhost",
+                          user=sys.argv[1],
+                          passwd=sys.argv[2],
+                          db=sys.argv[3],
+                          port=3306)
+    cursor = con.cursor()
+    cursor.execute("SELECT * from states WHERE name=%s ORDER BY id asc",
+                   (sys.argv[4], ))
+    rows = cursor.fetchall()
+    for row in rows:
+        print("{}".format(row))
