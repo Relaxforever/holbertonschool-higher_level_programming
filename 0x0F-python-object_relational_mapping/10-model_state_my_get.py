a127485fefa2ddd8869706bdb1d11282742b0156
Fidel Caicedo <869@holbertonschool.com>
2019-12-15 16:46:47 -0500

    uploading tenth point

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" this class gets the content of a table """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    getstate = session.query(State).filter_by(name=sys.argv[4]).first()
+    if (getstate):
+        print("{}".format(getstate.id))
+    else:
+        print("Not Found")
+    session.close()
