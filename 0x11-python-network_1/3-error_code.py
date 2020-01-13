e9d59b58301bea4db19b41d41cbfb8af77b45eaa
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 16:38:44 -0500

    adding third point

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,7 +5,7 @@ from urllib import request, error
 from sys import argv
 if __name__ == "__main__":
     try:
-        with request.Request(argv[1]) as response:
+        with request.urlopen(argv[1]) as response:
             print(response.read().decode("utf-8"))
     except error.HTTPError as answer:
         print("Error code: {}".format(answer.code))
