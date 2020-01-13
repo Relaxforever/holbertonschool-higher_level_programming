ce6598242a481830766d6bc182f6ac10b851889f
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 16:42:01 -0500

    adding third point

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # sends a POST request to the URL with the email,
-# and displays the body of the response
 from urllib import request, error
 from sys import argv
 if __name__ == "__main__":
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# sends a POST request to the URL with the email,
+# and displays the body of the response
