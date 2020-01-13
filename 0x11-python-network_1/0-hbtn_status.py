9436f7a28a80785eac4de158dfb40be52a2836ff
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 14:23:10 -0500

    cleaning whitespace and pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,6 @@
 from urllib import request
 with request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print("\t - type: {}".format(type(html)))
-    print("\t - content: {}".format(html))
-    print("\t - utf8 content: {}".format(html.decode('utf-8')))
-
-   
-
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
