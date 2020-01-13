bd4a1a758667e632542498dab2f7ca5d97be0127
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 14:55:43 -0500

    adding first point

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# Python script that takes in a URL, sends a
-# request to the URL and displays the value of the X-Request-Id
-# variable found in the header of the response.
+# takes in a URL sends request to the URL and displays the X-Request-Id
 from urllib import request
 from sys import argv
 with request.urlopen(argv[1]) as response:
