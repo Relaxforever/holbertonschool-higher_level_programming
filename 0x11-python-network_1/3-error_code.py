a3142a4c5270a39a9236eb7d2e00954b30a35d2c
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 16:31:29 -0500

    adding third point

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# sends a POST request to the URL with the email,
+# and displays the body of the response
+from urllib import request, error
+from sys import argv
+if __name__ == "__main__":
+    try:
+        with request.Request(argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as answer:
+        print("Error code: {}".format(answer.code))
