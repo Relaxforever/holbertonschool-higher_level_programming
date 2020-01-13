561b4e3e402255ff29a64144118669f7a2655125
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 17:03:51 -0500

    Adding if Conditional in case X-request doesnt exist

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,6 +3,4 @@ from requests import get
 from sys import argv
 if __name__ == "__main__":
     response = get(argv[1])
-    dict = response.headers
-    if dict['X-Request-Id']:
-        print(dict['X-Request-Id'])
+    print(response.headers.get("X-Request-Id"))
