5a4a3adcf1642c9c36acd469c0fdadd81685a3dc
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 15:35:52 -0500

    adding second point

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# sends a POST request to the URL with the email,
+# and displays the body of the response
+from urllib import request, parse
+from sys import argv
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        the_page = response.read()
