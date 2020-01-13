09d292ba9351e18aae93194ff7e0fc3d80ad8292
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 17:41:31 -0500

    Uploading seventh point

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,7 +3,7 @@ from requests import get, post
 from sys import argv
 if __name__ == "__main__":
     response = get(argv[1])
-    if response.status_code >= "400":
-        print(response.status_code)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
