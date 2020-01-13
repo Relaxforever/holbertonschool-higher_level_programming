0960b492401e672f2a94b740250fd0d51953f1e4
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 15:06:13 -0500

    adding first point, refactored and adding main ==

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,7 @@
 # takes in a URL sends request to the URL and displays the X-Request-Id
 from urllib import request
 from sys import argv
-with request.urlopen(argv[1]) as response:
-    header_dict = response.headers
-    print(header_dict.get('X-Request-Id'))
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as response:
+        header_dict = response.headers
+        print(header_dict.get('X-Request-Id'))
