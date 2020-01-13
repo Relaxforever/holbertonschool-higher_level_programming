0f95493e28fd8a92220a2c8de1d66917b4584e1c
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 14:19:25 -0500

    uploading first folder and first point

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Python script that fetches url and shows the body information of it.
+from urllib import request
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("\t - type: {}".format(type(html)))
+    print("\t - content: {}".format(html))
+    print("\t - utf8 content: {}".format(html.decode('utf-8')))
+
+   
+
