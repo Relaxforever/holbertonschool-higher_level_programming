9fb6d32eb79eebb65acf8071b2e52b3cea4022fc
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 14:24:39 -0500

    adding first line body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,7 @@
 from urllib import request
 with request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
+    print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf-8')))
