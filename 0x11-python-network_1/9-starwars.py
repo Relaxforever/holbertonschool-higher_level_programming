d9844c186076dadcc72418cd488b5b64a3f66ca6
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 22:59:47 -0500

    change print logic with get logic

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -2,9 +2,10 @@
 # get the info of star wars characters
 from requests import get, post
 from sys import argv
-response = get("https://swapi.co/api/people/?search={}".format(argv[1]))
-star_dict = response.json()
-print("Number of results: {}".format(star_dict['count']))
-results_list = star_dict['results']
-for character_dict in results_list:
-    print(character_dict.get('name'))
+if __name__ == "__main__":
+    response = get("https://swapi.co/api/people/?search={}".format(argv[1]))
+    star_dict = response.json()
+    print("Number of results: {}".format(star_dict['count']))
+    results_list = star_dict['results']
+    for character_dict in results_list:
+        print(character_dict.get('name'))
