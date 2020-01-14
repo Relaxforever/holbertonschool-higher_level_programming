c3458dd1ea8a78d71500d65246dff4b495649385
Fidel Caicedo <869@holbertonschool.com>
2020-01-13 22:54:31 -0500

    Uploading ninth point

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# get the info of star wars characters
+from requests import get, post
+from sys import argv
+response = get("https://swapi.co/api/people/?search={}".format(argv[1]))
+star_dict = response.json()
+print("Number of results: {}".format(star_dict['count']))
+results_list = star_dict['results']
+for character_dict in results_list:
+    for key, value in character_dict.items():
+        if key == 'name':
+            print(value)
