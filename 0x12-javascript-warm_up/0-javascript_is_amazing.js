c60d16e2ea69d38b38aaa32b79ec8c3b7f503bdb
Fidel Caicedo <869@holbertonschool.com>
2020-02-04 12:47:25 -0500

    uploading from 0 to 4 point

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+const hello = 'Javascript is amazing';
+console.log(hello);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const helloC = 'C is fun';
+const helloPy = 'Python is cool';
+const helloJs = 'Javascript is amazing';
+
+console.log(helloC);
+console.log(helloPy);
+console.log(helloJs);
