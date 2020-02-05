af08b51d8b5bfe2b475cbbaaa7a1abaed08c4601
Fidel Caicedo <869@holbertonschool.com>
2020-02-04 19:33:04 -0500

    uploading current points

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const helloC = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const hello in helloC) { console.log(helloC[hello]); }
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (Number(process.argv[2]))
+{
+  for (let cont = 0; cont < Number(process.argv[2]); cont++)
+  {
+    console.log('C is fun')
+  }
+}
+else
+  console.log('Missing number of occurrences')
+
