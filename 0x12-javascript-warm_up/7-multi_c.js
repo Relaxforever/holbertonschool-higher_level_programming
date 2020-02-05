32558503b6caad3fd868f5eb978dbf800a1325ad
Fidel Caicedo <869@holbertonschool.com>
2020-02-04 19:34:25 -0500

    uploading semistandard fixed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-if (Number(process.argv[2]))
-{
-  for (let cont = 0; cont < Number(process.argv[2]); cont++)
-  {
-    console.log('C is fun')
+if (Number(process.argv[2])) {
+  for (let cont = 0; cont < Number(process.argv[2]); cont++) {
+    console.log('C is fun');
   }
-}
-else
-  console.log('Missing number of occurrences')
-
+} else { console.log('Missing number of occurrences'); }
