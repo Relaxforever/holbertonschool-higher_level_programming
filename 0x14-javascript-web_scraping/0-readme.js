0c5856fe1c5835f8810ccd09b6186637cd664e9c
Fidel Caicedo <869@holbertonschool.com>
2020-02-11 14:05:56 -0500

    changing logic in conditionals

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], { encoding: 'utf-8' }, function readData (err, data) {
   if (err) {
-    console.log(data);
+    console.log(err);
   } else {
     console.log(data);
   }
